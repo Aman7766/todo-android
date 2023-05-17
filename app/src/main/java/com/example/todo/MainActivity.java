efbea23ac58e17b91c4de90135c38bc87aaac5ad
Amandeep <amankumar283@gmail.com>
2023-05-17 12:30:20 -0400

    ListView Working

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rv=findViewById(R.id.RV);
+
 
     }
 }
